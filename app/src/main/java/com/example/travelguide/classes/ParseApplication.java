f22ec1f2c31d50704c90dd7c182c41ffbb744119
Seun Adekunle <56938245+seunadekunle@users.noreply.github.com>
2021-07-13 18:08:07 -0400

    Integrated Parse SDK

---
+++ app/src/main/java/com/example/travelguide/classes/ParseApplication.java
@@ -0,0 +1,22 @@
+package com.example.travelguide.classes;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f")
+                .clientKey("rWFPEbTs7UzkaVsIXnQ4qmmr9oWqwXfiiJehtIZu")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
